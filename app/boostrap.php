2317c507d1ceb62fb3f73581022c255595322a47
Alex Vela <alexvela@Alexs-MacBook-Pro.local>
2023-10-24 12:23:13 -0500

    initial commit

---
+++ app/boostrap.php
@@ -0,0 +1,11 @@
+<?php
+    // Load Config
+
+    require_once 'config/config.php';
+
+
+    // Autoload core libraries
+    spl_autoload_register(function($classname){
+        require_once 'libraries/' . $classname . '.php';
+    });
+    
---
+++ app/views/inc/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.2.1/css/fontawesome.min.css" integrity="sha384-QYIZto+st3yW+o8+5OHfT6S482Zsvz2WfOzpFSXMF9zqeLcFV0/wlZpMtyFcZALm" crossorigin="anonymous">
+    <link rel="stylesheet" href="<?php echo URLROOT; ?>/public/css/style.css">
+    <title><?php echo SITENAME; ?></title>
+</head>
+<body>
+    <?php require APPROOT . '/views/inc/navbar.php' ?>
+    <div class="container">
+    
